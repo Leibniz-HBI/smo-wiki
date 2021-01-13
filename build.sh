0a0b19603b9d3d53af830bb5e256f518126ccf53
FlxVctr <flxvctr@protonmail.com>
2021-01-13 19:15:18 +0100

    updating markdowns

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 WIKI_URL=https://github.com/Leibniz-HBI/Social-Media-Observatory.wiki.git
 WIKI_DIR=Social-Media-Observatory.wiki
 git submodule update --init --remote
-cp "$WIKI_DIR"/*md ./
+cp "$WIKI_DIR" ./wiki
 git add -A
 git commit -a -m "updating markdowns"
 git push

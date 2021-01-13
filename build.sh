4109825f73278359a4e5681e49b12a8674d45385
FlxVctr <flxvctr@protonmail.com>
2021-01-13 19:30:41 +0100

    updating markdowns

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 WIKI_DIR=Social-Media-Observatory.wiki
 git pull
 git submodule update --init --remote
-cp docs/config.yml ./
+cp docs/_config.yml ./
 rm -rf docs
 cp -r "$WIKI_DIR" ./docs
-mv config.yml docs
+mv _config.yml docs
 rm docs/.git
 git add -A
 git commit -a -m "updating markdowns"

3d9880f34001fe374ca20633c2426fa6257548bb
FlxVctr <flxvctr@protonmail.com>
2021-01-14 12:03:15 +0100

    updating markdowns

--- convert_links.py
+++ convert_links.py
@@ -7,7 +7,7 @@ for file in glob('docs/*.md'):
     with open(file, 'r') as f:
         text = f.read()
     
-    replaced = re.sub('\[\[(.+)\]\]', '[\\1](\\1)', text)
+    replaced = re.sub(r'\[\[(.+)\]\]', r'[\1](\1)', text)
     
     with open(file, 'w') as f:
         f.write(replaced)

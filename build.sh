826d1cc3dffe8988a0e010d6337019be8ab0c639
Khandoker09 <63535729+Khandoker09@users.noreply.github.com>
2021-02-03 13:33:40 +0100

    push manually

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
-WIKI_DIR=Social-Media-Observatory.wiki
 git submodule update --init --remote # update Wiki-submodule (TODO: make optional)
 cp docs/_config.yml ./ # back up _config.yml for Github pages
 cp docs/Gemfile* ./ # back up  for Github pages
 cp -r docs/_layouts ./ # back up website layout
 cp -r docs/assets ./ # back up CSS
 rm -rf docs # delete old files
-cp -r "$WIKI_DIR" ./docs # write new files from Wiki-submodule
+cp -r Social-Media-Observatory.wiki ./docs # write new files from Wiki-submodule
 mv _config.yml docs/ # move back backuped files
 mv Gemfile* docs/ # move back backuped files
 mv _layouts docs/ # move back backuped files
@@ -16,4 +15,4 @@ python convert_links.py # convert [[wiki links]] to [wiki links](wiki-links)
 python generate_titles_from_filename.py # use the file-name to add # file name as title to md files
 git add -A # add updates
 git commit -a -m "updating markdowns" # commit updates
-git push # push updates
+# git push # push updates

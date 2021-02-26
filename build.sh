118b3536700d9f488f9fcb453896d80ee45d966f
FlxVctr <flxvctr@protonmail.com>
2021-02-26 12:40:23 +0100

    updating markdowns

--- build.sh
+++ build.sh
@@ -3,12 +3,16 @@ cp docs/_config.yml ./ # back up _config.yml for Github pages
 cp docs/Gemfile* ./ # back up  for Github pages
 cp -r docs/_layouts ./ # back up website layout
 cp -r docs/assets ./ # back up CSS
+cp docs/CNAME ./
+cp docs/google*.html ./
 rm -rf docs # delete old files
 cp -r Social-Media-Observatory.wiki ./docs # write new files from Wiki-submodule
 mv _config.yml docs/ # move back backuped files
 mv Gemfile* docs/ # move back backuped files
 mv _layouts docs/ # move back backuped files
 mv assets docs/ # move back backuped files
+mv CNAME docs/
+mv google*.html docs/
 rm docs/.git # remove repository files from docs folder so that it is not recognised as another submodule
 mv docs/Home.md docs/index.md # rename, so that Jekyll uses it as homepage
 python3 convert_links.py # convert [[wiki links]] to [wiki links](wiki-links)

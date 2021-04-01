3ab7b55f09e96defbc3e3f05219da8d8cf8b1224
FlxVctr <flxvctr@protonmail.com>
2021-04-01 22:33:48 +0200

    fix formatting authors

--- build.sh
+++ build.sh
@@ -22,10 +22,11 @@ python3 convert_links.py # convert [[wiki links]] to [wiki links](wiki-links)
 python3 generate_titles_from_filename.py # use the file-name to add # file name as title to md files
 cd Social-Media-Observatory.wiki
 echo -e "# Authorship Statistics\n" > ../docs/authors.md
-echo -e "## Authors by number of commits (edits)\n" >> ../docs/authors.md
+echo -e "## Authors by number of commits (edits)\n\n\`\`\`" >> ../docs/authors.md
 git shortlog -sn >> ../docs/authors.md
-echo -e "\n## Authors by number of lines\n" >> ../docs/authors.md
+echo -e "\`\`\`\n\n## Authors by number of lines\n\n\`\`\`" >> ../docs/authors.md
 git ls-files | while read f; do git blame -w -M -C -C --line-porcelain "$f" | grep -I '^author '; done | sort -f | uniq -ic | sort -n --reverse >> ../docs/authors.md
+echo -e "\`\`\`" >> ../docs/authors.md
 git add -A # add updates
 # git commit -a -m "updating markdowns" # commit updates
 # git push # push updates
